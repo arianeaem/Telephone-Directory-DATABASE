b271c6eb4d2700a4d0d75e5aa8c47a2f27299adf
arianeaem <gustoariane@gmail.com>
2025-06-15 08:21:35 +0800

    changed middleName to middle

--- telephonedb.sql
+++ telephonedb.sql
@@ -3,7 +3,7 @@ USE telephonedb;
 CREATE TABLE directory (
     lastName VARCHAR(50),
     firstName VARCHAR(50),
-    middleName VARCHAR(50),
+    middle VARCHAR(50),
     address VARCHAR(100),
     phoneNumber VARCHAR(15),
     PRIMARY KEY (lastName, firstName, middleName)
